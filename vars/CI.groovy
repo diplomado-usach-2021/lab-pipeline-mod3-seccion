42f6e7a4ab423585345a5b3e3e8de9b711e93102
victor <victor.menares.sapiain@gmail.com>
2022-02-01 23:55:35 -0300

    prueba

--- vars/CI.groovy
+++ vars/CI.groovy
@@ -92,7 +92,7 @@ def call(listaEtapas){
                                       //  sh " git branch -d release-v1-0-1 "
           
 
-                                        sh "  git ls-remote  --heads origin  release-v1-0-1-1-0 && git branch -d origin release-v1-0-1-1-0  "
+                                        sh "  git ls-remote  --heads origin  release-v1-0-1-1-0 &&  git push origin --delete release-v1-0-1-1-0  "
                                         sh " git branch release-v1-0-1-1-0 "
                                         sh " git push origin release-v1-0-1-1-0  "
                                       
