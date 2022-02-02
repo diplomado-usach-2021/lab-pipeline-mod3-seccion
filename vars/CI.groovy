74825bf7f9d36dbafab7ec7b51f1c112ab8b4d73
victor <victor.menares.sapiain@gmail.com>
2022-02-01 23:06:39 -0300

    prueba

--- vars/CI.groovy
+++ vars/CI.groovy
@@ -88,7 +88,11 @@ def call(listaEtapas){
                                         figlet "Stage: ${env.STAGE_NAME}"
                                          STAGE = env.STAGE_NAME
                                         figlet "Stage: ${env.STAGE_NAME}"
+                                      
                                       //  sh " git branch -d release-v1-0-1 "
+                           
+
+                                        sh "git ls-remote --exit-code --heads origin release-v1-0-1-1-1"
                                         sh " git branch release-v1-0-1-1-1 "
                                         sh " git push origin release-v1-0-1-1-1  "
                                       
